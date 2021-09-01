4100392a6d6e4d1e8f9e2ad4007698ec389ef6dc
RomelVazquez2008 <55199380+RomelVazquez2008@users.noreply.github.com>
2021-09-01 11:56:33 -0500

    Ahora los semaforos funcionan, solo falta hacer que el carro detecte un solo semaforo

--- Draft Vehiculos.py
+++ Draft Vehiculos.py
@@ -21,6 +21,9 @@ import IPython
 from random import choice
 from IPython.display import HTML
 
+def funcionVacia():
+    return True
+
 def llenarTuplaPosicionesCarros(cCarros):
     global SIETE, OCHO, CERO, QUINCE
     contador=0
@@ -83,9 +86,9 @@ class agenteSemaforo(ap.Agent):
         self.grid = self.model.grid
         self.random = self.model.random
         
-    def setupParameters(self, posX, posY, tiempoEspera, tiempoActivo, tiempoAmarillo, luzVerde, luzAmarillo, luzRojo):
-        self.posX=posX #int
-        self.posY=posY #int
+    def setupParameters(self, coordenada, tiempoEspera, tiempoActivo, luzVerde, luzRojo):
+        self.posX=coordenada[0] #int
+        self.posZ=coordenada[1] #int
         self.tiempoEspera=tiempoEspera #int (segundos) 
         self.tiempoActivo=tiempoActivo #int (segundos)
         self.luzVerde=luzVerde #bool
@@ -111,8 +114,22 @@ class agenteSemaforo(ap.Agent):
     
     #def encenderLuzRoja():
             
-    def prenderSemaforo():
-        self.actualizarTiempos(cantidadDeVehiculos)
+    def actualizarLuces(self):
+        if(self.luzRojo):
+            self.tiempoEspera=self.tiempoEspera-1
+            if(self.tiempoEspera==0):
+                self.tiempoActivo=5
+                self.luzVerde=True
+                self.luzRojo=False
+            
+        else:
+            self.tiempoActivo=self.tiempoActivo-1
+            if(self.tiempoActivo==0):
+                self.tiempoEspera=15
+                self.luzVerde=False
+                self.luzRojo=True
+            
+        #self.actualizarTiempos(cantidadDeVehiculos)
         #self.encenderLuzVerde()
         #self.encenderLuzAmarilla()
         #self.encenderLuzRoja()
@@ -126,20 +143,17 @@ class agenteVehiculo(ap.Agent):
         # Inicializar un agente con parametros
 
         
-    def setupParameters(self, velocidad, coordenada):
-        self.velocidad = velocidad #bool 
+    def setupParameters(self, coordenada):
         self.posX=coordenada[0] #int
         self.posY=0 #int
         self.posZ=coordenada[1] #int
         self.posXInicial=coordenada[0] #int nunca cambia
         self.posZInicial=coordenada[1] #int nunca cambia
+        self.primerSemaforo=True
+        self.vueltas=0
         #if(primerCarro(self.posXInicial, self.posZInicial, self.posX)):
             #self.actualizarPosicion()
-        
-
-    def modificarVelocidad(self, velocidad):
-        #acelerar o frenar, depende el caso
-        self.velocidad=velocidad
+    
     
     def actualizarPosicion(self):
         #Ir a la izquierda
@@ -191,6 +205,56 @@ class agenteVehiculo(ap.Agent):
                     if(self.posZ-i.posZ==-1):
                         return True
         return False
+    
+    def verSemaforo(self):
+        if(self.primerSemaforo):
+            agentes=self.grid.neighbors(self, distance=1)
+            for i in agentes:
+                if(str(type(i))=="<class '__main__.agenteSemaforo'>"):
+                    if(i.luzRojo):
+                        return True
+        return False
+    def end(self):
+        global QUINCE, OCHO, SIETE, CERO
+        if(carroIzquierda(self.posXInicial, self.posZInicial, 1)): #ej self(11,6) i(10,6)
+            if(self.posX==CERO):
+                self.posX=QUINCE
+                self.posZ=OCHO
+                self.grid.move_to(self, (QUINCE,OCHO))
+                self.vueltas=self.vueltas+1
+                self.primerSemaforo=True
+                return True
+                    
+                #ir hacia abajo
+        elif(carroAbajo(self.posXInicial, self.posZInicial, 1)): #eje self (0,1) i(0,0)
+            if(self.posZ==CERO):
+                self.posX=SIETE
+                self.posZ=QUINCE
+                self.grid.move_to(self, (SIETE,QUINCE))
+                self.vueltas=self.vueltas+1
+                self.primerSemaforo=True
+                return True
+                    
+                #ir a la derecha 
+        elif(carroDerecha(self.posXInicial, self.posZInicial, 1)): #ej self(0,0) i(1,0)
+            if(self.posX==QUINCE):
+                self.posX=CERO
+                self.posZ=SIETE
+                self.grid.move_to(self, (CERO,SIETE))
+                self.vueltas=self.vueltas+1
+                self.primerSemaforo=True
+                return True
+                    
+                #ir hacia arriba
+        elif(carroArriba(self.posXInicial, self.posZInicial, 1)): #
+            if(self.posZ==QUINCE):
+                self.posX=OCHO
+                self.posZ=CERO
+                self.grid.move_to(self, (OCHO,CERO))
+                self.vueltas=self.vueltas+1
+                self.primerSemaforo=True
+                return True
+        return False
         
         
         #DIFERENCIA ENTRE MOVE TO Y MOVE BY
@@ -206,14 +270,6 @@ class agenteVehiculo(ap.Agent):
        # elif (semaforo.light == 2 or semaforo.light == 3):
             #modificarVelocidad(self,0)
         
-    #quitar la comunicacion
-    #def detectarVehiculos(self, agenteVehiculo):
-        #definimos la distancia entre dos coches
-        #distanciaX=abs(self.posX-agenteVehiculo.posX)
-        #distanciaZ=abs(self.posZ-agenteVehiculo.posZ)
-        #en el caso de luz 1=on, 0=off
-        #if (distanciaX<=10 or luz==1 or distanciaZ<=10):
-            #modificarVelocidad(self,0)
     
 
 class modeloVehiculo(ap.Model):
@@ -225,56 +281,76 @@ class modeloVehiculo(ap.Model):
         global YSEMAFORO
         s= self.p.size
         origenCarros=llenarTuplaPosicionesCarros(self.p.agentsCarro)
+        cantidadCarros=self.p.agentsCarro
         
         self.grid = ap.Grid(self, (s,s), track_empty=True)
         
         self.vehiculos = ap.AgentList(self, self.p.agentsCarro, agenteVehiculo)
         self.grid.add_agents(self.vehiculos, positions=origenCarros) #DEFINIR LAS POSICIONES INICIALES DE LOS AGENTES
         
-        contador=0
-        for i in self.grid.positions:
-            self.vehiculos[contador].setupParameters(0,self.grid.positions[i])
-            contador=contador+1
-        
         self.semaforos = ap.AgentList(self, self.p.agentsSemaforo, agenteSemaforo)
         self.grid.add_agents(self.semaforos, positions=[(XSEMAFORO,XSEMAFORO),(XSEMAFORO,YSEMAFORO),(YSEMAFORO,XSEMAFORO),(YSEMAFORO,YSEMAFORO)])
+        
+        contador=0
+        condicion=True
+        for i in self.grid.positions:
+            if(condicion):
+                self.vehiculos[contador].setupParameters(self.grid.positions[i])
+                if(contador==cantidadCarros-1):
+                    condicion=False
+                    contador=-1
+            else:
+                if(contador==0):
+                    self.semaforos[contador].setupParameters(self.grid.positions[i], contador*5, 5, True, False) #como tiempo de espera es 0, tiene que estar prendida la luz verde
+                else:
+                    self.semaforos[contador].setupParameters(self.grid.positions[i], contador*5, 5, False, True)
+            contador=contador+1
     
 
     def step(self):
         # Called at every simulation step
         #self.agents.leerSemaforo(agenteSemaforo)
         #self.agents.detectarVehiculos(agenteVehiculo)
-        nueva_velocidad=random.randrange(10)
-        self.vehiculos.modificarVelocidad(nueva_velocidad)
         for i in self.grid.positions:
             if(str(type(i))=="<class '__main__.agenteVehiculo'>"):
-                if(i.choque()):
-                    print("no pasa nada")
+                if(i.end()==False):
+                #Falta meter un if para cuando el carro llegue a la interseccion y ponga self.primerSemaforo=false
+                    if (i.choque() or i.verSemaforo()):
+                        funcionVacia()
+                    else:
+                        i.actualizarPosicion()
                 else:
-                    i.actualizarPosicion()
+                    print("")
+                    print("Contador vueltas: ",i.vueltas)
+            elif(str(type(i))=="<class '__main__.agenteSemaforo'>"):
+                i.actualizarLuces()
         #IMPRESION DE LAS POSICIONES DE LOS AGENTES
         for i in self.grid.positions:
-            print (self.grid.positions[i])
+            if(str(type(i))=="<class '__main__.agenteVehiculo'>"):
+                print (self.grid.positions[i])
+            #if(str(type(i))=="<class '__main__.agenteSemaforo'>"):
+                #print (i.tiempoActivo)
     
         
     def update(self):
+        self.vehiculos.record('vueltas', self.vehiculos.vueltas)
         self.vehiculos.record('posX', self.vehiculos.posX)  # Record variable
         self.vehiculos.record('posZ', self.vehiculos.posZ)
 
     def end(self):
         # del agents
         # Called at the end of the simulation
+        self.report('Vueltas al circuito', self.vehiculos.vueltas)
         self.report('Distancia recorrida por los vehiculos en el eje x', self.vehiculos.posX)
         self.report('Distancia recorrida por los vehiculos en el eje z', self.vehiculos.posZ)# Report a simulation result
         
 parameters = {
     'size':16,
-    'velocidad':0,
     'posX':0,
     'posZ':0,
-    'agentsCarro':20,
+    'agentsCarro':1,
     'agentsSemaforo':4,
-    'steps':10,
+    'steps':200,
 }
 
 
